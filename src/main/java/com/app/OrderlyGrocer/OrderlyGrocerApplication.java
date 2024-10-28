76768ad638f4c4fb282353750853bb20054a422c
Vaibhav <vaibhavanandav@gmail.com>
2024-10-29 04:08:23 +0530

    Startup

---
+++ src/main/java/com/app/OrderlyGrocer/OrderlyGrocerApplication.java
@@ -0,0 +1,13 @@
+package com.app.OrderlyGrocer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderlyGrocerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderlyGrocerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/OrderlyGrocer/OrderlyGrocerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.OrderlyGrocer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderlyGrocerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
